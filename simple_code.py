075b894281623cc8868abeddec19b474431137a9
Chelsea Uju <chelsea.uju@gmail.com>
2025-10-31 20:01:11 +0100

    Add files via upload

---
+++ simple_code.py
@@ -0,0 +1,32 @@
+team_tyrosine = [
+    {
+        "name": "Chelsea U",
+        "slack_username": "Chels123",
+        "country": "Germany",
+        "hobby": "traveling",
+        "affiliation": "Hackbio Intern",
+        "fave_gene": "ATG"
+    },
+    {
+        "name": "David O",
+        "slack_username": "Dav123",
+        "country": "Nigeria",
+        "hobby": "cooking",
+        "affiliation": "Hackbio Intern",
+        "fave_gene": "TAG"
+    },
+    {
+        "name": "Christopher A",
+        "slack_username": "Chris123",
+        "country": "Austria",
+        "hobby": "football",
+        "affiliation": "Hackbio Intern",
+        "fave_gene": "TAA"
+    }
+]
+
+for member in team_tyrosine:
+    print(
+        f"Name: {member['name']}, Username: {member['slack_username']}, Country: {member['country']}, "
+        f"Hobby: {member['hobby']}, Affiliation: {member['affiliation']}, Favourite gene: {member['fave_gene']}"
+    )
